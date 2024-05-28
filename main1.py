2bae225d2e9729d05354afbbe3cd0e5f8f8df52d
GregoryBucks <eduhu4ka1@gmail.com>
2024-05-28 10:01:53 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,20 @@
+a='123456787654321'
+print(len(a))       # 1
+first=2
+second=8
+summa=first + second
+diff=first - second
+print(summa , diff)        # 2
+first=3
+second=5
+third=10
+mean=(first + second + third)/3
+print(mean)           #3
+first_string='Tuesday'
+second_string='Monday'
+print(second_string+', '+first_string)        #4
+a=5
+b=7
+c=9
+f=(a*b)+(a*c)
+print(f ** 3 / 2)
