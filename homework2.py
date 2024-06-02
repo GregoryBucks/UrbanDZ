65a8c76335c66074bfc4d4105221bcef01aa9d9d
GregoryBucks <eduhu4ka1@gmail.com>
2024-06-02 12:17:07 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kol_vo_DZ=12
+kol_vo_4asov=1.5
+kurs=('Python')
+odno_zadanie=kol_vo_4asov / kol_vo_DZ
+print('Курс:' ,kurs+',','Всего задач:',kol_vo_DZ,',','Затрачен часов:',kol_vo_4asov,',','среднее время выплнения',odno_zadanie,'часа')
